f6803f027c7ec18c0bba364cbf5a8de4c000ef84
Conor <ckosidowski11@winona.edu>
2017-02-20 22:32:36 -0600

    Changed the colors a bit Added a generic class to allow complex objects to be passed through rest calls

--- src/main/webapp/resources/scripts/root.js
+++ src/main/webapp/resources/scripts/root.js
@@ -1,7 +1,7 @@
 angular.module("root", [])
     .controller("hello", function ($scope, $http) {
-        $http.get("http://localhost:8080/services/").then(function (response) {
-            $scope.greeting = response.data;
+        $http.get("/services/").then(function (response) {
+            $scope.greeting = response.data.data;
         }, function (response) {
             $scope.greeting = "Something went wrong: " + response;
         });
