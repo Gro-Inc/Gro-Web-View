5f6c56de7e3ffb03ec6011ec1c3ebd249660779f
Conor <ckosidowski11@winona.edu>
2017-02-20 20:27:41 -0600

    Added an example request to the REST client from the view

--- src/main/webapp/resources/scripts/root.js
+++ src/main/webapp/resources/scripts/root.js
@@ -1,1 +1,8 @@
-angular.module("root", []);
+angular.module("root", [])
+    .controller("hello", function ($scope, $http) {
+        $http.get("http://localhost:8080/services/").then(function (response) {
+            $scope.greeting = response.data;
+        }, function (response) {
+            $scope.greeting = "Something went wrong: " + response;
+        });
+    });
